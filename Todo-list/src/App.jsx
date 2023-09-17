9796abca80172757fcb5986f76da004c9e120db4
Ussayed Shakeel <TTS72I@tr.pte.hu>
2023-09-17 13:19:38 +0200

    Commit

--- Todo-list/src/App.jsx
+++ Todo-list/src/App.jsx
@@ -3,14 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import CssBaseline from "@mui/material/CssBaseline"
 import './App.css'
-
+import TodoList from './TodoList'
+import Navbar from './Navbar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
     <CssBaseline/>
+    <Navbar/>
     <h1>Todos</h1>
+    <TodoList/>
     </>
   )
 }
---
+++ Todo-list/src/TodoList.jsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import List from '@mui/material/List';
+
+import { useState,useEffect } from 'react';
+import TodoItem from './TodoItem';
+import TodoForm from './TodoForm';
+import { Box,Typography } from '@mui/material';
+const getInitialData=()=>{
+    const data =JSON.parse(localStorage.getItem("todos"));
+    if(!data) return[];
+    return data
+
+};
+
+export default function TodoList() {
+
+
+
+
+const [todos,setTodos]=useState(getInitialData);
+
+useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+},[todos])
+
+const removeTodo = (id)=>{
+    setTodos(prevTodos =>{
+        return prevTodos.filter((t)=>t.id !== id)
+    })
+}
+const toggleTodo=(id)=>{
+    setTodos(prevTodos=>{
+        return prevTodos.map(todo=>{
+            if(todo.id===id){
+                return {...todo,completed: ! todo.completed}
+            }else{
+                return todo
+            }
+        })
+    })
+}
+
+const addTodo=(text)=>{
+    setTodos((prevTodos)=>{
+        return [...prevTodos,{text:text, id:crypto.randomUUID(), completed:false}]
+    })
+
+}
+
+return(
+    <Box sx={{display:"flex",
+    justifyContent:"center",
+    flexDirection:"column",
+    alignItems:"center",
+}}
+    >
+    
+    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        News
+        </Typography>
+        
+<List sx={{width:'100%', maxWidth: 360, bgcolor:'background.paper'}}>
+{todos.map(todo=>(
+    <TodoItem todo={todo} key={todo.id} removeTodo={()=> removeTodo(todo.id)} toggle={()=> toggleTodo(todo.id)} />
+   ))}
+   <TodoForm addTodo={addTodo}/>
+    </List>
+    </Box>
+)}
